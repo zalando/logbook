df7c9af15bd8c5af308acb99f42ee6166ab975c9
Willi Schönborn <willi.schoenborn@zalando.de>
2015-12-04 16:40:15 +0100

    Added classifier and correlation id as first line to http style formatted output
    Together with the logging writer this should produce nicer output, as the first line is still part of the same line as the metadata of the log message itself,
    placing all of the http output to the left, without any prefixing

--- logbook-core/src/test/java/org/zalando/logbook/DefaultHttpLogFormatterTest.java
+++ logbook-core/src/test/java/org/zalando/logbook/DefaultHttpLogFormatterTest.java
@@ -45,7 +45,8 @@ public final class DefaultHttpLogFormatterTest {
 
         final String http = unit.format(new SimplePrecorrelation<>(correlationId, request));
 
-        assertThat(http, equalTo("GET /test?limit=1 HTTP/1.1\n" +
+        assertThat(http, equalTo("Request: c9408eaa-677d-11e5-9457-10ddb1ee7671\n" +
+                "GET /test?limit=1 HTTP/1.1\n" +
                 "Accept: application/json\n" +
                 "Content-Type: text/plain\n" +
                 "\n" +
@@ -64,7 +65,8 @@ public final class DefaultHttpLogFormatterTest {
 
         final String http = unit.format(new SimplePrecorrelation<>(correlationId, request));
 
-        assertThat(http, equalTo("GET /test HTTP/1.1\n" +
+        assertThat(http, equalTo("Request: 2bd05240-6827-11e5-bbee-10ddb1ee7671\n" +
+                "GET /test HTTP/1.1\n" +
                 "Accept: application/json\n" +
                 "Content-Type: text/plain\n" +
                 "\n" +
@@ -81,7 +83,8 @@ public final class DefaultHttpLogFormatterTest {
 
         final String http = unit.format(new SimplePrecorrelation<>(correlationId, request));
 
-        assertThat(http, equalTo("GET /test HTTP/1.1\n" +
+        assertThat(http, equalTo("Request: 0eae9f6c-6824-11e5-8b0a-10ddb1ee7671\n" +
+                "GET /test HTTP/1.1\n" +
                 "Accept: application/json"));
     }
 
@@ -96,7 +99,8 @@ public final class DefaultHttpLogFormatterTest {
 
         final String http = unit.format(new SimpleCorrelation<>(correlationId, request, response));
 
-        assertThat(http, equalTo("HTTP/1.1 200\n" +
+        assertThat(http, equalTo("Response: 2d51bc02-677e-11e5-8b9b-10ddb1ee7671\n" +
+                "HTTP/1.1 200\n" +
                 "Content-Type: application/json\n" +
                 "\n" +
                 "{\"success\":true}"));
@@ -112,7 +116,8 @@ public final class DefaultHttpLogFormatterTest {
 
         final String http = unit.format(new SimpleCorrelation<>(correlationId, request, response));
 
-        assertThat(http, equalTo("HTTP/1.1 200\n" +
+        assertThat(http, equalTo("Response: 3881ae92-6824-11e5-921b-10ddb1ee7671\n" +
+                "HTTP/1.1 200\n" +
                 "Content-Type: application/json"));
     }
 

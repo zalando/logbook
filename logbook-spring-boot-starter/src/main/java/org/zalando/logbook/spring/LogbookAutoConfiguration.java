7b47e7d663e67d14fc2b0adf6ca82b29928ab97c
Willi Schönborn <willi.schoenborn@zalando.de>
2016-07-09 20:34:35 +0200

    Adder proper precondition for auto configuration
    This fixes #119

--- logbook-spring-boot-starter/src/main/java/org/zalando/logbook/spring/LogbookAutoConfiguration.java
+++ logbook-spring-boot-starter/src/main/java/org/zalando/logbook/spring/LogbookAutoConfiguration.java
@@ -21,7 +21,6 @@ package org.zalando.logbook.spring;
  */
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.http.HttpRequestInterceptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,6 +30,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.SecurityFilterAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
 import org.springframework.boot.context.embedded.FilterRegistrationBean;
@@ -71,7 +71,11 @@ import static javax.servlet.DispatcherType.REQUEST;
 @Configuration
 @ConditionalOnClass(Logbook.class)
 @EnableConfigurationProperties(LogbookProperties.class)
-@AutoConfigureAfter({WebMvcAutoConfiguration.class, SecurityFilterAutoConfiguration.class})
+@AutoConfigureAfter({
+        JacksonAutoConfiguration.class,
+        SecurityFilterAutoConfiguration.class,
+        WebMvcAutoConfiguration.class,
+})
 @Import(SecurityLogbookAutoConfiguration.class)
 public class LogbookAutoConfiguration {
 

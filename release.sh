2541da604f3cf96afe7bbcee1a105fde41a8139d
Willi Schönborn <willi.schoenborn@zalando.de>
2016-08-30 17:11:41 +0200

    Silent release script

--- release.sh
+++ release.sh
@@ -1,17 +1,17 @@
-#!/usr/bin/env bash
+#!/bin/sh -e
 
-set -e
+: ${2?"Usage: $0 <release-version> <next-version>"}
 
 mvn scm:check-local-modification
 
 # release
-mvn versions:set
-git add pom.xml */pom.xml
-git commit
+mvn versions:set -D newVersion=$1
+git add $(find . -name pom.xml)
+git commit -m "Release $1"
 mvn clean deploy -P release
 mvn scm:tag
 
 # next development version
-mvn versions:set
-git add pom.xml */pom.xml
-git commit
+mvn versions:set -D newVersion=$2-SNAPSHOT
+git add $(find . -name pom.xml)
+git commit -m "Development $2-SNAPSHOT"

c176571ec8cb7222c768e94d7df30d44e3afcef7
Alexander Yastrebov <alexander.yastrebov@zalando.de>
2017-03-22 16:49:21 +0100

    GH-148 Implement minChunkSize/maxChunkSize support

--- logbook-core/src/main/java/org/zalando/logbook/StringSpliterator.java
+++ logbook-core/src/main/java/org/zalando/logbook/StringSpliterator.java
@@ -6,12 +6,28 @@ import java.util.function.Consumer;
 class StringSpliterator implements Spliterator<String> {
 
     private final String string;
-    private final int size;
+    private final int minChunkSize;
+    private final int maxChunkSize;
     private int position;
 
-    StringSpliterator(final String string, final int size) {
+    StringSpliterator(final String string, final int maxChunkSize) {
+        this(string, maxChunkSize, maxChunkSize);
+    }
+
+    StringSpliterator(final String string, final int minChunkSize, final int maxChunkSize) {
+        if (maxChunkSize <= 0) {
+            throw new IllegalArgumentException("maxChunkSize is expected to be greater than zero");
+        }
+        if (minChunkSize <= 0) {
+            throw new IllegalArgumentException("minChunkSize is expected to be greater than zero");
+        }
+        if (minChunkSize > maxChunkSize) {
+            throw new IllegalArgumentException("minChunkSize is expected to be less or equal to " + maxChunkSize);
+        }
+
         this.string = string;
-        this.size = size;
+        this.minChunkSize = minChunkSize;
+        this.maxChunkSize = maxChunkSize;
     }
 
     @Override
@@ -19,8 +35,12 @@ class StringSpliterator implements Spliterator<String> {
         if (position >= string.length()) {
             return false;
         }
-        action.accept(string.substring(position, Math.min(string.length(), position + size)));
-        position += size;
+
+        final String chunk = nextChunk();
+
+        action.accept(chunk);
+
+        position += chunk.length();
         return true;
     }
 
@@ -31,11 +51,32 @@ class StringSpliterator implements Spliterator<String> {
 
     @Override
     public long estimateSize() {
+        final int size = minChunkSize + (maxChunkSize - minChunkSize) / 2;
         return string.length() / size + Integer.signum(string.length() % size);
     }
 
     @Override
     public int characteristics() {
-        return IMMUTABLE | NONNULL | ORDERED | SIZED;
+        return IMMUTABLE | NONNULL | ORDERED | (minChunkSize == maxChunkSize ? SIZED : 0);
+    }
+
+    private String nextChunk() {
+        final int chunkMax = position + maxChunkSize;
+        if (chunkMax >= string.length()) {
+            return string.substring(position);
+        }
+
+        final int chunkMin = position + minChunkSize;
+        for (int i = chunkMax; i >= chunkMin; i--) {
+            final char ch = string.charAt(i - 1);
+            if (isSplitCharacter(ch)) {
+                return string.substring(position, i);
+            }
+        }
+        return string.substring(position, chunkMax);
+    }
+
+    private static boolean isSplitCharacter(final char ch) {
+        return ch == ' ' || ch == ',' || ch == ':';
     }
 }
--- logbook-core/src/test/java/org/zalando/logbook/StringSpliteratorTest.java
+++ logbook-core/src/test/java/org/zalando/logbook/StringSpliteratorTest.java
@@ -1,12 +1,18 @@
 package org.zalando.logbook;
 
+import java.util.List;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import static java.util.Arrays.asList;
+import static java.util.Spliterator.SIZED;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.StreamSupport.stream;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(MockitoJUnitRunner.class)
 public final class StringSpliteratorTest {
@@ -23,6 +29,62 @@ public final class StringSpliteratorTest {
 
     @Test
     public void shouldNotSupportPartitions() {
-        assertThat(new StringSpliterator("", 0).trySplit(), is(nullValue()));
+        assertThat(new StringSpliterator("", 1).trySplit(), is(nullValue()));
+    }
+
+    @Test
+    public void shouldBeSizedWhenMinEqualToMax() {
+        assertTrue((new StringSpliterator("Hello", 5).characteristics() | SIZED) != 0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldFailWhenMinIsZero() {
+        new StringSpliterator("whatever", 0, 10);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldFailWhenMaxIsZero() {
+        new StringSpliterator("whatever", 10, 0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldFailWhenMinGreaterThanMax() {
+        new StringSpliterator("whatever", 11, 10);
+    }
+
+    @Test
+    public void shouldSplitAfterSplitCharacter() {
+        assertThat(split("12345 67890", 5, 6), is(asList("12345 ", "67890")));
+        assertThat(split("12345:67890", 5, 6), is(asList("12345:", "67890")));
+        assertThat(split("12345,67890", 5, 6), is(asList("12345,", "67890")));
+    }
+
+    @Test
+    public void shouldSplitAfterSplitCharacterWhenChunkLenghtIsMinimal() {
+        assertThat(split("12345 67890", 6, 7), is(asList("12345 ", "67890")));
+    }
+
+    @Test
+    public void shouldSplitOnMaxWhenNoSplitCharacterPresent() {
+        assertThat(split("123456 789012", 5, 6), is(asList("123456", " 78901", "2")));
+    }
+
+    @Test
+    public void shouldNotSplitWhenMaxIsEqualToLength() {
+        assertThat(split("123 45", 1, 6), is(asList("123 45")));
+    }
+
+    @Test
+    public void shouldNotSplitWhenMaxIsGreaterThanLength() {
+        assertThat(split("123 45", 1, 10), is(asList("123 45")));
+    }
+
+    @Test
+    public void shouldSplitWithMinimalChunkLenOfOne() {
+        assertThat(split(" space", 1, 5), is(asList(" ", "space")));
+    }
+
+    private static List<String> split(final String string, final int min, final int max) {
+        return stream(new StringSpliterator(string, min, max), false).collect(toList());
     }
 }

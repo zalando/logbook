0f35dd6882f0c8536980167b17218b919d6c38ba
Willi Schönborn <willi.schoenborn@zalando.de>
2018-04-19 22:42:35 +0200

    Moved to semantic release script

--- release.sh
+++ release.sh
@@ -1,20 +1,24 @@
 #!/bin/sh -ex
 
-: ${2?"Usage: $0 <release-version> <next-version>"}
+: ${1?"Usage: $0 <major|minor|patch>"}
 
 ./mvnw scm:check-local-modification
 
+current=$(git describe --abbrev=0)
+release=$(semver -i $1 ${current})
+next=$(semver -i minor ${release})
+
 # release
-./mvnw versions:set -D newVersion=$1
+./mvnw versions:set -D newVersion=${release}
 git add $(find . -name pom.xml)
-git commit -m "Release $1"
+git commit -m "Release ${release}"
 ./mvnw clean deploy -P release
 ./mvnw scm:tag
 
 # next development version
-./mvnw versions:set -D newVersion=$2-SNAPSHOT
+./mvnw versions:set -D newVersion=${next}-SNAPSHOT
 git add $(find . -name pom.xml)
-git commit -m "Development $2-SNAPSHOT"
+git commit -m "Development ${next}-SNAPSHOT"
 
 git push
 git push --tags

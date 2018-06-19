125f75aa2d0fc0e77c01bf1cdc4882aa29d1ceca
Willi Schönborn <willi.schoenborn@zalando.de>
2018-06-20 01:49:42 +0200

    Updated release script

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -ex
 
-: ${1?"Usage: $0 <major|minor|patch>"}
+: ${1?"Usage: $0 <[pre]major|[pre]minor|[pre]patch|prerelease>"}
 
 ./mvnw scm:check-local-modification
 
@@ -8,17 +8,12 @@ current=$(git describe --abbrev=0 || echo 0.0.0)
 release=$(semver ${current} -i $1 --preid RC)
 next=$(semver ${release} -i minor)
 
-# release
 ./mvnw versions:set -D newVersion=${release}
-git add $(find . -name pom.xml)
-git commit -m "Release ${release}"
-./mvnw clean deploy -P release
-./mvnw scm:tag
+git commit -am "Release ${release}"
+./mvnw clean deploy scm:tag -P release -D tag=${release} -D pushChanges=false
 
-# next development version
 ./mvnw versions:set -D newVersion=${next}-SNAPSHOT
-git add $(find . -name pom.xml)
-git commit -m "Development ${next}-SNAPSHOT"
+git commit -am "Development ${next}-SNAPSHOT"
 
 git push
 git push --tags

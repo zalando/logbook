b74ca9357c59e40cbd1784816861ca7504e10f90
Thomas Skjølberg <thomas.skjolberg@gmail.com>
2019-05-21 17:57:48 +0200

    Unit test and benchmark for JsonMediaType

---
+++ logbook-jmh/src/main/java/org/zalando/logbook/json/JsonMediaTypeBenchmark.java
@@ -0,0 +1,48 @@
+package org.zalando.logbook.json;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+@Fork(value = 1, warmups = 1)
+@Warmup(iterations = 2, time = 3, timeUnit = TimeUnit.SECONDS)
+@BenchmarkMode(Mode.Throughput)
+@Measurement(iterations = 5, time = 3, timeUnit = TimeUnit.SECONDS)
+public class JsonMediaTypeBenchmark {
+
+    @Benchmark
+    public Object contentType1() throws IOException {
+        return JsonMediaType.JSON.test("application/json");
+    }
+
+    @Benchmark
+    public Object contentType2() throws IOException {
+        return JsonMediaType.JSON.test("application/json;charset=utf-8");
+    }
+
+    @Benchmark
+    public Object contentType3() throws IOException {
+        return JsonMediaType.JSON.test("application/abc+json");
+    }
+
+    @Benchmark
+    public Object contentType4() throws IOException {
+        return JsonMediaType.JSON.test("application/abc+json;charset=utf-8");
+    }
+
+    public static void main(String[] args) throws RunnerException {
+        Options options = new OptionsBuilder().include(JsonMediaTypeBenchmark.class.getSimpleName())
+                .forks(1).build();
+        new Runner(options).run();
+    }    
+}
---
+++ logbook-json/src/test/java/org/zalando/logbook/json/JsonMediaTypeTest.java
@@ -0,0 +1,29 @@
+package org.zalando.logbook.json;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class JsonMediaTypeTest {
+
+    @Test
+    public void testApplicationJson1() {
+        assertTrue(JsonMediaType.JSON.test("application/json"));
+    }
+    
+    @Test
+    public void testApplicationJson2() {
+        assertTrue(JsonMediaType.JSON.test("application/abc+json;charset=utf-8"));
+    }
+
+    @Test
+    public void testApplicationJson1WithCharset() {
+        assertTrue(JsonMediaType.JSON.test("application/json;charset=utf-8"));
+    }
+    
+    @Test
+    public void testApplicationJson2WithCharset() {
+        assertTrue(JsonMediaType.JSON.test("application/abc+json;charset=utf-8"));
+    }
+}

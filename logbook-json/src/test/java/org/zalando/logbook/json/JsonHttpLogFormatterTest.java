339dfed7f966d740b6b40b5f604238b84490f571
Thomas Skjølberg <thomas.skjolberg@gmail.com>
2019-05-21 19:10:05 +0200

    Ugly JsonHttpLogFormatter rewrite

--- logbook-json/src/test/java/org/zalando/logbook/json/JsonHttpLogFormatterTest.java
+++ logbook-json/src/test/java/org/zalando/logbook/json/JsonHttpLogFormatterTest.java
@@ -15,6 +15,12 @@ import java.io.IOException;
 import java.time.Clock;
 import java.time.Duration;
 import java.time.Instant;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.TreeMap;
 
 import static com.jayway.jsonassert.JsonAssert.with;
 import static java.time.Clock.systemUTC;
@@ -26,6 +32,7 @@ import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.hasKey;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.emptyCollectionOf;
 import static org.hamcrest.Matchers.not;
 import static org.zalando.logbook.Origin.LOCAL;
 import static org.zalando.logbook.Origin.REMOTE;
@@ -225,7 +232,7 @@ final class JsonHttpLogFormatterTest {
 
         final String json = unit.format(new SimplePrecorrelation("", systemUTC()), request);
 
-        assertThat(json, containsString("\"body\":}"));
+        assertThat(json, not(containsString("\"body\":}")));
     }
 
     @Test
@@ -323,7 +330,7 @@ final class JsonHttpLogFormatterTest {
 
         final String json = unit.format(new SimpleCorrelation(correlationId, ZERO), response);
 
-        assertThat(json, containsString("\"body\":}"));
+        assertThat(json, not(containsString("\"body\":}")));
     }
 
     @Test
@@ -349,7 +356,136 @@ final class JsonHttpLogFormatterTest {
 
         assertThat(json, containsString("{\"name\":\"Bob\"\n;};"));
     }
+    
+    @Test
+    void shouldLogCorrectRequestOrigin() throws IOException {
+        final String correlationId = "53de2640-677d-11e5-bc84-10ddb1ee7671";
+        final HttpRequest local = MockHttpRequest.create().withOrigin(LOCAL);
+        final String localJson = unit.format(new SimplePrecorrelation(correlationId, systemUTC()), local);
 
+        with(localJson)
+                .assertThat("$.origin", is("local"));
+        
+        final HttpRequest remote = MockHttpRequest.create().withOrigin(REMOTE);
+        final String remoteJson = unit.format(new SimplePrecorrelation(correlationId, systemUTC()), remote);
+
+        with(remoteJson)
+                .assertThat("$.origin", is("remote"));
+        
+    }       
+    
+    @Test
+    void shouldLogCorrectResponseOrigin() throws IOException {
+        final String correlationId = "53de2640-677d-11e5-bc84-10ddb1ee7671";
+        final HttpResponse local = MockHttpResponse.create().withOrigin(LOCAL);
+        final String localJson = unit.format(new SimpleCorrelation(correlationId, ofMillis(125)), local);
+
+        with(localJson)
+                .assertThat("$.origin", is("local"));
+        
+        final HttpResponse remote = MockHttpResponse.create().withOrigin(REMOTE);
+        final String remoteJson = unit.format(new SimpleCorrelation(correlationId, ofMillis(125)), remote);
+
+        with(remoteJson)
+                .assertThat("$.origin", is("remote"));
+        
+    }
+    
+    @Test
+    void shouldWorkWithSpecialCharacters() throws IOException {
+        final String correlationId = "5478b8da-6d87-11e5-a80f-10ddb1ee7671";
+        final HttpResponse response = MockHttpResponse.create()
+                .withContentType("application/json")
+                .withHeaders(MockHeaders.of("X-Nordic-Text", "ØÆÅabc\\\""))
+                .withBodyAsString("{\"name\":\"Bob\"}");
+
+        final String json = unit.format(new SimpleCorrelation(correlationId, ZERO), response);
+
+        with(json)
+            .assertThat("$.headers['X-Nordic-Text']", is(singletonList("ØÆÅabc\\\"")));
+    }
+    
+    @Test
+    void shouldWorkLogEmptyHeaders() throws IOException {
+        final String correlationId = "5478b8da-6d87-11e5-a80f-10ddb1ee7671";
+        
+        Map<String, List<String>> headers = new TreeMap<>();
+        headers.put("Content-Type", Arrays.asList("application/json"));
+        headers.put("X-Empty-Header", Collections.emptyList());
+        
+        final HttpResponse response = MockHttpResponse.create()
+                .withContentType("application/json")
+                .withHeaders(headers)
+                .withBodyAsString("{\"name\":\"Bob\"}");
+
+        final String json = unit.format(new SimpleCorrelation(correlationId, ZERO), response);
+
+        with(json)
+            .assertThat("$.headers['X-Empty-Header']", is(emptyCollectionOf(List.class)));
+    }
+
+    @Test
+    void shouldLogNonstandardHttpPort() throws IOException {
+        final String correlationId = "3ce91230-677b-11e5-87b7-10ddb1ee7671";
+        final HttpRequest request = MockHttpRequest.create()
+                .withProtocolVersion("HTTP/1.0")
+                .withOrigin(REMOTE)
+                .withPort(Optional.of(123))
+                .withPath("/test");
+
+        final String json = unit.format(new SimplePrecorrelation(correlationId, systemUTC()), request);
+
+        with(json)
+                .assertThat("$.uri", is("http://localhost:123/test"));
+    }
+    
+    @Test
+    void shouldLogNonstandardHttpsPort() throws IOException {
+        final String correlationId = "3ce91230-677b-11e5-87b7-10ddb1ee7671";
+        final HttpRequest request = MockHttpRequest.create()
+                .withProtocolVersion("HTTPS/1.0")
+                .withScheme("https")
+                .withOrigin(REMOTE)
+                .withPort(Optional.of(123))
+                .withPath("/test");
+
+        final String json = unit.format(new SimplePrecorrelation(correlationId, systemUTC()), request);
+
+        with(json)
+                .assertThat("$.uri", is("https://localhost:123/test"));
+    }    
+
+    @Test
+    void shouldNotLogStandardHttpsPort() throws IOException {
+        final String correlationId = "3ce91230-677b-11e5-87b7-10ddb1ee7671";
+        final HttpRequest request = MockHttpRequest.create()
+                .withProtocolVersion("HTTPS/1.0")
+                .withScheme("https")
+                .withOrigin(REMOTE)
+                .withPort(Optional.of(443))
+                .withPath("/test");
+
+        final String json = unit.format(new SimplePrecorrelation(correlationId, systemUTC()), request);
+
+        with(json)
+                .assertThat("$.uri", is("https://localhost/test"));
+    }    
+
+    @Test
+    void shouldNotLogStandardHttpPort() throws IOException {
+        final String correlationId = "3ce91230-677b-11e5-87b7-10ddb1ee7671";
+        final HttpRequest request = MockHttpRequest.create()
+                .withProtocolVersion("HTTP/1.0")
+                .withScheme("http")
+                .withOrigin(REMOTE)
+                .withPort(Optional.of(80))
+                .withPath("/test");
+
+        final String json = unit.format(new SimplePrecorrelation(correlationId, systemUTC()), request);
+
+        with(json)
+                .assertThat("$.uri", is("http://localhost/test"));
+    }     
     static class SimplePrecorrelation implements Precorrelation {
 
         private final String id;

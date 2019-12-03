2f27699bafc58fc622c8ffd25f7f7f4fe35d651f
Willi Schönborn <w.schoenborn@gmail.com>
2019-12-03 17:36:14 +0100

    Added query replace predicate support
    Fixes #658

--- logbook-core/src/main/java/org/zalando/logbook/QueryFilters.java
+++ logbook-core/src/main/java/org/zalando/logbook/QueryFilters.java
@@ -2,9 +2,11 @@ package org.zalando.logbook;
 
 import org.apiguardian.api.API;
 
+import java.util.function.Predicate;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import static java.util.regex.Pattern.quote;
+import static java.util.regex.Pattern.compile;
 import static org.apiguardian.api.API.Status.EXPERIMENTAL;
 import static org.apiguardian.api.API.Status.MAINTAINED;
 import static org.apiguardian.api.API.Status.STABLE;
@@ -28,17 +30,65 @@ public final class QueryFilters {
     }
 
     @API(status = MAINTAINED)
-    public static QueryFilter replaceQuery(final String name, final String replacement) {
-        final Pattern pattern = Pattern.compile("((?:^|&)" + quote(name) + "=)(?:.*?)(&|$)");
-        final String replacementPattern = "$1" + replacement + "$2";
+    public static QueryFilter replaceQuery(
+            final String name, final String replacement) {
 
-        return query -> pattern.matcher(query).replaceAll(replacementPattern);
+        return replaceQuery(name::equals, replacement);
+    }
+
+    @API(status = EXPERIMENTAL)
+    public static QueryFilter replaceQuery(
+            final Predicate<String> predicate, final String replacement) {
+
+        final Pattern pattern = compile("(?<name>[^&]*?)=(?:[^&]*)");
+
+        return query -> {
+            final Matcher matcher = pattern.matcher(query);
+            final StringBuffer result = new StringBuffer(query.length());
+
+            while (matcher.find()) {
+                if (predicate.test(matcher.group("name"))) {
+                    matcher.appendReplacement(result, "${name}");
+                    result.append('=');
+                    result.append(replacement);
+                } else {
+                    matcher.appendReplacement(result, "$0");
+                }
+            }
+            matcher.appendTail(result);
+
+            return result.toString();
+        };
     }
 
     @API(status = EXPERIMENTAL)
     public static QueryFilter removeQuery(final String name) {
-        final Pattern pattern = Pattern.compile("((?:^|&)" + quote(name) +   "=[^&]*)");
-        return query -> pattern.matcher(query).replaceAll("").replaceFirst("^&", "");
+        final Predicate<String> predicate = name::equals;
+        final Pattern pattern = compile("&?(?<name>[^&]+?)=(?:[^&]*)");
+
+        return query -> {
+            final Matcher matcher = pattern.matcher(query);
+            final StringBuffer result = new StringBuffer(query.length());
+
+            while (matcher.find()) {
+                if (predicate.test(matcher.group("name"))) {
+                    matcher.appendReplacement(result, "");
+                } else {
+                    matcher.appendReplacement(result, "$0");
+                }
+            }
+            matcher.appendTail(result);
+
+            final String output = result.toString();
+
+            if (output.startsWith("&")) {
+                // ideally this case would be covered by the regex, but
+                // I wasn't able to make it work
+                return output.substring(1);
+            }
+
+            return output;
+        };
     }
 
 }
--- logbook-core/src/test/java/org/zalando/logbook/QueryFiltersTest.java
+++ logbook-core/src/test/java/org/zalando/logbook/QueryFiltersTest.java
@@ -1,11 +1,16 @@
 package org.zalando.logbook;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
+import java.util.Set;
+
+import static com.google.common.collect.Sets.newTreeSet;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.zalando.logbook.QueryFilters.defaultValue;
+import static org.zalando.logbook.QueryFilters.replaceQuery;
 
 final class QueryFiltersTest {
 
@@ -13,19 +18,47 @@ final class QueryFiltersTest {
     void accessTokenShouldFilterAccessTokenParameterByDefault() {
         final QueryFilter unit = defaultValue();
 
-        assertThat(unit.filter("name=alice&access_token=bob"), is(equalTo("name=alice&access_token=XXX")));
+        assertThat(unit.filter("name=alice&access_token=bob"),
+                is("name=alice&access_token=XXX"));
     }
 
-    @Test
-    void shouldRemoveGivenQueryParameters() {
+    @CsvSource({
+            "name=Alice,name=XXX",
+            "name=Alice&name=Bob,name=XXX&name=XXX",
+            "name=Alice&active=true,name=XXX&active=true",
+            "active&name=Alice,active&name=XXX",
+            "name=Alice&active&age=5,name=XXX&active&age=5",
+            "name=Alice&secret=123,name=XXX&secret=XXX",
+    })
+    @ParameterizedTest
+    void shouldReplaceMatchingQueryParameters(
+            final String query, final String expected) {
+
+        final Set<String> properties = newTreeSet(String::compareToIgnoreCase);
+        properties.add("SECRET");
+        properties.add("name");
+
+        final QueryFilter unit = replaceQuery(properties::contains, "XXX");
+
+        assertThat(unit.filter(query), is(expected));
+    }
+
+    @CsvSource({
+            "q=boots&sort=price&direction=asc,sort=price&direction=asc",
+            "sort=price&direction=asc&q=boots,sort=price&direction=asc",
+            "sort=price&q=boots&direction=asc,sort=price&direction=asc",
+            "sort=price&direction=asc,sort=price&direction=asc",
+            "q=boots&test=true&q=boots,test=true",
+            "q=1&q=2&q=3,''",
+            "'',''"
+    })
+    @ParameterizedTest
+    void shouldRemoveGivenQueryParameters(
+            final String query, final String expected) {
+
         final QueryFilter unit = QueryFilters.removeQuery("q");
 
-        assertThat(unit.filter("q=boots&sort=price&direction=asc"), is("sort=price&direction=asc"));
-        assertThat(unit.filter("sort=price&direction=asc&q=boots"), is("sort=price&direction=asc"));
-        assertThat(unit.filter("sort=price&q=boots&direction=asc"), is("sort=price&direction=asc"));
-        assertThat(unit.filter("sort=price&direction=asc"), is("sort=price&direction=asc"));
-        assertThat(unit.filter("q=boots&suq=true&q=boots"), is("suq=true"));
-        assertThat(unit.filter("q=1&q=2&q=3"), is(""));
-        assertThat(unit.filter(""), is(""));
+        assertThat(unit.filter(query), is(expected));
     }
+
 }

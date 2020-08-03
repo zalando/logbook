d0b995689f01a5efccb04d91f985311964e341aa
Jan Pascal Maas <5416345+jpmaas@users.noreply.github.com>
2020-08-03 16:03:00 +0200

    Remove final from formRequestMode property. Fixes #797

--- logbook-spring-boot-autoconfigure/src/main/java/org/zalando/logbook/autoconfigure/LogbookProperties.java
+++ logbook-spring-boot-autoconfigure/src/main/java/org/zalando/logbook/autoconfigure/LogbookProperties.java
@@ -39,7 +39,7 @@ public final class LogbookProperties {
     @Getter
     @Setter
     public static class Filter {
-        private final FormRequestMode formRequestMode = FormRequestMode.fromProperties();
+        private FormRequestMode formRequestMode = FormRequestMode.fromProperties();
     }
 
 }

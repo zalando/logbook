cf9bab7a16f95c6dcdbc1c3732d28cfccb46369f
Willi Schönborn <w.schoenborn@gmail.com>
2021-03-18 12:27:23 +0100

    Fixed release script (#981)

--- release.sh
+++ release.sh
@@ -1,24 +1,24 @@
 #!/bin/sh -ex
 
-: ${1?"Usage: $0 <[pre]major|[pre]minor|[pre]patch|prerelease>"}
+: "${1?"Usage: $0 <[pre]major|[pre]minor|[pre]patch|prerelease>"}"
 
 ./mvnw scm:check-local-modification
 
-current=$(git describe --abbrev=0 || echo 0.0.0)
-release=$(semver ${current} -i $1 --preid RC)
-next=$(semver ${release} -i minor)
+current=$({ echo 0.0.0; git tag --list --sort=version:refname; } | tail -n1)
+release=$(semver "${current}" -i "$1" --preid RC)
+next=$(semver "${release}" -i minor)
 
-git checkout -b release/${release}
+git checkout -b "release/${release}"
 
-./mvnw versions:set -D newVersion=${release}
+./mvnw versions:set -D newVersion="${release}"
 git commit -am "Release ${release}"
-./mvnw clean deploy scm:tag -P release -D tag=${release} -D pushChanges=false -D skipTests -D dependency-check.skip
+./mvnw clean deploy scm:tag -P release -D tag="${release}" -D pushChanges=false -D skipTests -D dependency-check.skip
 
-./mvnw versions:set -D newVersion=${next}-SNAPSHOT
+./mvnw versions:set -D newVersion="${next}-SNAPSHOT"
 git commit -am "Development ${next}-SNAPSHOT"
 
 git push
 git push --tags
 
 git checkout main
-git branch -D release/${release}
+git branch -D "release/${release}"

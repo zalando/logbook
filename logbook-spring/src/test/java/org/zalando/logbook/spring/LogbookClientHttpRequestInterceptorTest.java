7647b7ee3f4bac57aaddad108b56a64e7f328cc5
Nicolas Noffke <nicolas.noffke@storecast.de>
2021-05-19 13:43:29 +0200

    wrap client http response body in buffered input stream to support mark/reset (#963)

--- logbook-spring/src/test/java/org/zalando/logbook/spring/LogbookClientHttpRequestInterceptorTest.java
+++ logbook-spring/src/test/java/org/zalando/logbook/spring/LogbookClientHttpRequestInterceptorTest.java
@@ -1,5 +1,7 @@
 package org.zalando.logbook.spring;
 
+import com.github.tomakehurst.wiremock.WireMockServer;
+import java.io.IOException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,8 +10,7 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.HttpMethod;
-import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.zalando.logbook.Correlation;
@@ -19,23 +20,24 @@ import org.zalando.logbook.HttpLogWriter;
 import org.zalando.logbook.Logbook;
 import org.zalando.logbook.Precorrelation;
 import org.zalando.logbook.TestStrategy;
-
-import java.io.IOException;
-
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.client.ExpectedCount.once;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
-import static org.springframework.test.web.client.response.MockRestResponseCreators.withBadRequest;
-import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
 @ExtendWith(MockitoExtension.class)
 class LogbookClientHttpRequestInterceptorTest {
+
+    private final WireMockServer server = new WireMockServer(options().dynamicPort());
 
     @Mock
     private HttpLogWriter writer;
@@ -53,34 +55,30 @@ class LogbookClientHttpRequestInterceptorTest {
     private ArgumentCaptor<Correlation> correlationCaptor;
 
     private RestTemplate restTemplate;
-    private MockRestServiceServer serviceServer;
-    private Logbook logbook;
-
-    private LogbookClientHttpRequestInterceptor interceptor;
 
     @BeforeEach
     void setup() {
+        server.start();
         when(writer.isActive()).thenReturn(true);
-        logbook = Logbook.builder()
+        Logbook logbook = Logbook.builder()
                 .strategy(new TestStrategy())
                 .sink(new DefaultSink(new DefaultHttpLogFormatter(), writer))
                 .build();
-        interceptor = new LogbookClientHttpRequestInterceptor(logbook);
-        restTemplate = new RestTemplate();
+        LogbookClientHttpRequestInterceptor interceptor = new LogbookClientHttpRequestInterceptor(logbook);
+        restTemplate = new RestTemplate(new HttpComponentsClientHttpRequestFactory());
         restTemplate.getInterceptors().add(interceptor);
-        serviceServer = MockRestServiceServer.createServer(restTemplate);
     }
 
     @AfterEach
-    void mockHttpVerify() {
-        serviceServer.verify();
+    void tearDown() {
+        server.stop();
     }
 
     @Test
     void get200() throws IOException {
-        serviceServer.expect(once(), requestTo("/test/get")).andExpect(method(HttpMethod.GET))
-                .andRespond(withSuccess().body("response"));
-        restTemplate.getForObject("/test/get", String.class);
+        server.stubFor(get("/test/get/withcontent").willReturn(aResponse().withStatus(200).withBody("response")));
+
+        restTemplate.getForObject(server.baseUrl() + "/test/get/withcontent", String.class);
 
         verify(writer).write(precorrelationCaptor.capture(), requestCaptor.capture());
         verify(writer).write(correlationCaptor.capture(), responseCaptor.capture());
@@ -98,34 +96,28 @@ class LogbookClientHttpRequestInterceptorTest {
 
     @Test
     void get200WithEmptyResponseBody(){
-        serviceServer.expect(once(), requestTo("/test/get")).andExpect(method(HttpMethod.GET))
-                .andRespond(withSuccess());
-
-        restTemplate.getForObject("/test/get", Void.class);
+        server.stubFor(get("/test/get/withoutcontent").willReturn(aResponse().withStatus(200)));
+        restTemplate.getForObject(server.baseUrl() + "/test/get/withoutcontent", Void.class);
     }
 
     @Test
     void get200WithNonEmptyResponseBody() {
-        String expectedResponseBody = "response";
-        serviceServer.expect(once(), requestTo("/test/get")).andExpect(method(HttpMethod.GET))
-                .andRespond(withSuccess().body(expectedResponseBody));
-
-        String actualResponseBody = restTemplate.getForObject("/test/get", String.class);
+        server.stubFor(get("/test/get/withcontent").willReturn(aResponse().withStatus(200).withBody("response")));
+        String actualResponseBody = restTemplate.getForObject(server.baseUrl() + "/test/get/withcontent", String.class);
 
         assertNotNull(actualResponseBody);
-        assertEquals(expectedResponseBody, actualResponseBody);
+        assertEquals("response", actualResponseBody);
     }
 
     @Test
     void post400() throws IOException {
-        serviceServer.expect(once(), requestTo("/test/post")).andExpect(method(HttpMethod.POST))
-                .andRespond(withBadRequest().body("response"));
-        assertThrows(HttpClientErrorException.class, () -> restTemplate.postForObject("/test/post", "request", Void.class));
+        server.stubFor(post("/test/post/withcontent").willReturn(aResponse().withStatus(400).withBody("response")));
+        assertThrows(HttpClientErrorException.class, () -> restTemplate.postForObject(server.baseUrl() + "/test/post/withcontent", "request", String.class));
 
         verify(writer).write(precorrelationCaptor.capture(), requestCaptor.capture());
         verify(writer).write(correlationCaptor.capture(), responseCaptor.capture());
 
-        assertTrue(requestCaptor.getValue().contains("/test/post"));
+        assertTrue(requestCaptor.getValue().contains("/test/post/withcontent"));
         assertTrue(requestCaptor.getValue().contains("POST"));
         assertTrue(requestCaptor.getValue().contains("Remote: localhost"));
         assertTrue(requestCaptor.getValue().contains(precorrelationCaptor.getValue().getId()));
@@ -135,5 +127,7 @@ class LogbookClientHttpRequestInterceptorTest {
         assertTrue(responseCaptor.getValue().contains(precorrelationCaptor.getValue().getId()));
         assertTrue(responseCaptor.getValue().contains("400 Bad Request"));
         assertTrue(responseCaptor.getValue().contains("response"));
+
+        server.verify(postRequestedFor(urlEqualTo("/test/post/withcontent")).withRequestBody(equalTo("request")));
     }
 }

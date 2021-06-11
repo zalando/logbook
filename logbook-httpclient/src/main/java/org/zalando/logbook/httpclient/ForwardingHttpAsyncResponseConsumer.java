3dfe8b1ad1241d375eba8a36969c466dbe010452
sokomishalov <sokomishalov@mail.ru>
2021-06-11 17:28:47 +0300

    "lombokify" logbook-httpclient a bit

--- logbook-httpclient/src/main/java/org/zalando/logbook/httpclient/ForwardingHttpAsyncResponseConsumer.java
+++ logbook-httpclient/src/main/java/org/zalando/logbook/httpclient/ForwardingHttpAsyncResponseConsumer.java
@@ -1,61 +1,11 @@
 package org.zalando.logbook.httpclient;
 
-import org.apache.http.HttpException;
-import org.apache.http.HttpResponse;
-import org.apache.http.nio.ContentDecoder;
-import org.apache.http.nio.IOControl;
+import lombok.experimental.Delegate;
 import org.apache.http.nio.protocol.HttpAsyncResponseConsumer;
-import org.apache.http.protocol.HttpContext;
-
-import java.io.IOException;
 
 abstract class ForwardingHttpAsyncResponseConsumer<T> implements HttpAsyncResponseConsumer<T> {
 
+    @Delegate
     protected abstract HttpAsyncResponseConsumer<T> delegate();
-
-    @Override
-    public void responseReceived(final HttpResponse response) throws IOException, HttpException {
-        delegate().responseReceived(response);
-    }
-
-    @Override
-    public void consumeContent(final ContentDecoder decoder, final IOControl control) throws IOException {
-        delegate().consumeContent(decoder, control);
-    }
-
-    @Override
-    public void responseCompleted(final HttpContext context) {
-        delegate().responseCompleted(context);
-    }
-
-    @Override
-    public boolean cancel() {
-        return delegate().cancel();
-    }
-
-    @Override
-    public boolean isDone() {
-        return delegate().isDone();
-    }
-
-    @Override
-    public T getResult() {
-        return delegate().getResult();
-    }
-
-    @Override
-    public void failed(final Exception e) {
-        delegate().failed(e);
-    }
-
-    @Override
-    public Exception getException() {
-        return delegate().getException();
-    }
-
-    @Override
-    public void close() throws IOException {
-        delegate().close();
-    }
 
 }

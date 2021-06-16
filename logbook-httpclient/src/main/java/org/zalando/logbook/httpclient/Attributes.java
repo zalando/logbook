f1626fcc93ac5d67a50f26f7e4fce48d5d3f5c95
Mikhael Sokolov <sokomishalov@mail.ru>
2021-06-16 09:37:44 +0300

    "lombokify" logbook-httpclient a bit (#1063)

--- logbook-httpclient/src/main/java/org/zalando/logbook/httpclient/Attributes.java
+++ logbook-httpclient/src/main/java/org/zalando/logbook/httpclient/Attributes.java
@@ -1,13 +1,11 @@
 package org.zalando.logbook.httpclient;
 
+import lombok.experimental.UtilityClass;
 import org.zalando.logbook.Logbook;
 
+@UtilityClass
 final class Attributes {
 
     static final String STAGE = Logbook.class.getName() + ".STAGE";
-
-    private Attributes() {
-
-    }
 
 }

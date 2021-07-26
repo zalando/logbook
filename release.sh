fd9f70824b2bb2ce32e7b7ca41309a0e66ce9842
Willi Schönborn <w.schoenborn@gmail.com>
2021-07-26 09:25:43 +0200

    Added changelog generator to release script

--- release.sh
+++ release.sh
@@ -1,6 +1,9 @@
-#!/bin/sh -ex
+#!/usr/bin/env bash
+
+set -euxo pipefail
 
 : "${1?"Usage: $0 <[pre]major|[pre]minor|[pre]patch|prerelease>"}"
+: "${CHANGELOG_GITHUB_TOKEN?"Needs CHANGELOG_GITHUB_TOKEN env var"}"
 
 ./mvnw scm:check-local-modification
 
@@ -15,6 +18,8 @@ git commit -am "Release ${release}"
 ./mvnw clean deploy scm:tag -P release -D tag="${release}" -D pushChanges=false -D skipTests -D dependency-check.skip
 
 ./mvnw versions:set -D newVersion="${next}-SNAPSHOT"
+docker run -it --rm -v "$(pwd)":/usr/local/src/your-app \
+    githubchangeloggenerator/github-changelog-generator -u zalando -p logbook
 git commit -am "Development ${next}-SNAPSHOT"
 
 git push

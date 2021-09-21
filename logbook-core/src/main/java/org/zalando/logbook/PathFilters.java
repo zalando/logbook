2759fcf2db023aaf495957f6dfdace8f55b3db33
Ismail Ismailov <ismail2ov@gmail.com>
2021-09-21 09:41:17 +0200

    Dynamic path parameters replacing of PathFilters

--- logbook-core/src/main/java/org/zalando/logbook/PathFilters.java
+++ logbook-core/src/main/java/org/zalando/logbook/PathFilters.java
@@ -2,6 +2,8 @@ package org.zalando.logbook;
 
 import org.apiguardian.api.API;
 
+import java.util.function.UnaryOperator;
+
 import static org.apiguardian.api.API.Status.EXPERIMENTAL;
 
 @API(status = EXPERIMENTAL)
@@ -29,6 +31,22 @@ public class PathFilters {
     @API(status = EXPERIMENTAL)
     public static PathFilter replace(final String expression, final String replacement) {
         return new DefaultPathFilter(replacement, expression);
+    }
+
+    /**
+     * Filter/replace by applying function. Accept filters in the form
+     *
+     * <br>
+     * <p>
+     * Where {@code secret} (including curly braces) gets replaced by value applying function.
+     *
+     * @param expression  filter expression
+     * @param replacementFunction function to apply for filtered segments
+     * @return a {@link PathFilter} that replaces segments
+     */
+    @API(status = EXPERIMENTAL)
+    public static PathFilter replace(final String expression, final UnaryOperator<String> replacementFunction) {
+        return new DynamicPathFilter(replacementFunction, expression);
     }
 
 }

3b32c83ec0813396f9a89b2947716f5ea6b3ea7f
Ismail Ismailov <ismail2ov@gmail.com>
2021-09-23 13:43:17 +0200

    Dynamic path parameters replacing of PathFilters (#1165)
    * Dynamic path parameters replacing of PathFilters
    
    * Add tests for dynamic path parameters replacing of PathFilters
    
    * Change test class to public
    
    * Remove unnecessary checks

---
+++ logbook-core/src/main/java/org/zalando/logbook/DynamicPathFilter.java
@@ -0,0 +1,64 @@
+package org.zalando.logbook;
+
+import java.util.Objects;
+import java.util.function.UnaryOperator;
+
+final class DynamicPathFilter implements PathFilter {
+
+    private final UnaryOperator<String> replacementFunction;
+    private final String[] parts;
+
+    DynamicPathFilter(final UnaryOperator<String> replacementFunction, final String pathExpression) {
+        this.replacementFunction = replacementFunction;
+
+        final String[] parts = pathExpression.split("/");
+
+        for (int i = 0; i < parts.length; i++) {
+            final String part = parts[i];
+            if (part.startsWith("{") && part.endsWith("}")) {
+                parts[i] = null;
+            }
+        }
+
+        this.parts = parts;
+    }
+
+
+    @Override
+    public String filter(final String path) {
+        final String[] pathParts = path.split("/");
+
+        if (parts.length > pathParts.length) {
+            return path;
+        }
+
+        final StringBuilder builder = new StringBuilder();
+
+        for (int i = 0; i < parts.length; i++) {
+            if (Objects.isNull(parts[i])) {
+                if (i > 0) {
+                    builder.append('/');
+                }
+                builder.append(replacementFunction.apply(pathParts[i]));
+            } else {
+                if (Objects.equals(parts[i], pathParts[i])) {
+                    if (!parts[i].isEmpty()) {
+                        builder.append('/');
+                    }
+                    builder.append(pathParts[i]);
+                } else {
+                    return path;
+                }
+            }
+        }
+
+        if (parts.length < pathParts.length) {
+            for (int i = parts.length; i < pathParts.length; i++) {
+                builder.append('/');
+                builder.append(pathParts[i]);
+            }
+        }
+
+        return builder.toString();
+    }
+}

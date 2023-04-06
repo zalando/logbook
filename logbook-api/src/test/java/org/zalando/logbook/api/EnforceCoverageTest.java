47c4b2bfe7dc89e03b7d686f29464eeedae2a9d8
Sadeq Dousti <3616518+msdousti@users.noreply.github.com>
2023-04-06 08:31:58 +0200

    Fix coverage

--- logbook-api/src/test/java/org/zalando/logbook/api/EnforceCoverageTest.java
+++ logbook-api/src/test/java/org/zalando/logbook/api/EnforceCoverageTest.java
@@ -3,7 +3,9 @@ package org.zalando.logbook.api;
 import com.google.gag.annotation.remark.Hack;
 import com.google.gag.annotation.remark.OhNoYouDidnt;
 import org.junit.jupiter.api.Test;
+import org.zalando.logbook.api.internal.ExceptionThrowingLogbookFactory;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.mock;
 
@@ -39,7 +41,21 @@ final class EnforceCoverageTest {
 
     @Test
     void fakeLogbookShouldThrow() {
+        Logbook logbook = Logbook.create();
+
         assertThrows(UnsupportedOperationException.class, () ->
-                Logbook.create().process(mock(HttpRequest.class)));
+                logbook.process(mock(HttpRequest.class)));
+        assertThrows(UnsupportedOperationException.class, () ->
+                logbook.process(mock(HttpRequest.class), mock(Strategy.class)));
+    }
+
+    @Test
+    void shouldHaveCorrectPriority() {
+        assertEquals(Integer.MIN_VALUE, ExceptionThrowingLogbookFactory.INSTANCE.getPriority());
+
+        // Create a LogbookFactory with default getPriority()
+        LogbookFactory logbookFactory = (condition, correlationId, queryFilter, pathFilter,
+                                         headerFilter, bodyFilter, requestFilter, responseFilter, strategy, sink) -> null;
+        assertEquals(0, logbookFactory.getPriority());
     }
 }

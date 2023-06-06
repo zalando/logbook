9dff58a00bd1d2dc953c7b5968be353a95483e6d
arowe <arowe@adaptivebiotech.com>
2023-06-06 03:30:30 -0700

    fix netty sequence ability to continue logging reads and writes to duplex channel

--- logbook-netty/src/test/java/org/zalando/logbook/netty/SequenceTest.java
+++ logbook-netty/src/test/java/org/zalando/logbook/netty/SequenceTest.java
@@ -14,6 +14,17 @@ class SequenceTest {
     private final Runnable second = mock(Runnable.class, "second");
     private final Runnable third = mock(Runnable.class, "third");
 
+    private final Runnable fourth = mock(Runnable.class, "fourth");
+    private final Runnable fifth = mock(Runnable.class, "fifth");
+    private final Runnable sixth = mock(Runnable.class, "sixth");
+
+    private final Runnable seventh = mock(Runnable.class, "seventh");
+    private final Runnable eighth = mock(Runnable.class, "eighth");
+    private final Runnable ninth = mock(Runnable.class, "ninth");
+
+    private final Runnable tenth = mock(Runnable.class, "tenth");
+    private final Runnable eleventh = mock(Runnable.class, "eleventh");
+
     private final Sequence unit = new Sequence(3);
 
     @Test
@@ -41,6 +52,44 @@ class SequenceTest {
         inOrder.verify(first).run();
         inOrder.verify(second).run();
         inOrder.verify(third).run();
+    }
+
+    @Test
+    void runsMoreEagerlyAllowsSequenceReuse() {
+        // reusing the slots of the sequence array should allow reuse
+        unit.set(2, ninth);
+        unit.set(1, eighth);
+        unit.set(0, seventh);
+
+        unit.set(2, sixth);
+        unit.set(1, fifth);
+        unit.set(0, fourth);
+
+        unit.set(2, third);
+        unit.set(1, second);
+        unit.set(0, first);
+
+        // still doesn't run partial sequence when reused
+        unit.set(2, tenth);
+        unit.set(1, eleventh);
+
+        final InOrder inOrder1 = inOrder(seventh, eighth, ninth);
+        final InOrder inOrder2 = inOrder(fourth, fifth, sixth);
+        final InOrder inOrder3 = inOrder(first, second, third);
+
+        inOrder3.verify(first).run();
+        inOrder3.verify(second).run();
+        inOrder3.verify(third).run();
+        inOrder2.verify(fourth).run();
+        inOrder2.verify(fifth).run();
+        inOrder2.verify(sixth).run();
+        inOrder1.verify(seventh).run();
+        inOrder1.verify(eighth).run();
+        inOrder1.verify(ninth).run();
+
+        // these two shouldn't run because they are missing their 3rd companion
+        verify(tenth, never()).run();
+        verify(eleventh, never()).run();
     }
 
 }

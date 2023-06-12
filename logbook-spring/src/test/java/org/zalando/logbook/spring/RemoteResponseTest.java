c20c51f558a998451da08b17115fcebc4bc6b285
Khacharia Dipesh <Dipesh.Khacharia@swisscom.com>
2023-06-12 18:52:52 +0200

    fix test coverage

--- logbook-spring/src/test/java/org/zalando/logbook/spring/RemoteResponseTest.java
+++ logbook-spring/src/test/java/org/zalando/logbook/spring/RemoteResponseTest.java
@@ -33,6 +33,15 @@ class RemoteResponseTest {
     }
 
     @Test
+    @SuppressWarnings("deprecation")
+    void getRawStatusThrows() throws IOException {
+        ClientHttpResponse response = mock(ClientHttpResponse.class);
+        when(response.getStatusCode()).thenThrow(new NoSuchMethodError());
+        when(response.getRawStatusCode()).thenThrow(new IOException("io exception"));
+        assertThatThrownBy(() -> new RemoteResponse(response).getStatus()).hasMessageContaining("io exception");
+    }
+
+    @Test
     void defaultBody() throws IOException {
         assertThat(unit(helloWorld()).getBody()).asString().isEqualTo("");
     }

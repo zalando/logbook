a81085906b26316bcad2bae94c4d3abb8e7c68b3
Sadeq Dousti <3616518+msdousti@users.noreply.github.com>
2023-06-26 00:17:30 +0200

    Migrate to SLF4j 2.x

--- logbook-core/src/test/java/org/zalando/logbook/core/DefaultHttpLogWriterTest.java
+++ logbook-core/src/test/java/org/zalando/logbook/core/DefaultHttpLogWriterTest.java
@@ -1,45 +1,52 @@
 package org.zalando.logbook.core;
 
-import org.junit.jupiter.api.AfterEach;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.read.ListAppender;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.slf4j.LoggerFactory;
 import org.zalando.logbook.HttpLogWriter;
 import org.zalando.logbook.Logbook;
 import org.zalando.logbook.core.DefaultLogbook.SimpleCorrelation;
 import org.zalando.logbook.core.DefaultLogbook.SimplePrecorrelation;
-import uk.org.lidalia.slf4jtest.LoggingEvent;
-import uk.org.lidalia.slf4jtest.TestLogger;
-import uk.org.lidalia.slf4jtest.TestLoggerFactory;
 
 import java.io.IOException;
 import java.time.Clock;
+import java.util.List;
 
-import static com.google.common.collect.Iterables.getOnlyElement;
+import static ch.qos.logback.classic.Level.INFO;
+import static ch.qos.logback.classic.Level.TRACE;
 import static java.time.Instant.MIN;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static uk.org.lidalia.slf4jext.Level.INFO;
-import static uk.org.lidalia.slf4jext.Level.TRACE;
 
 final class DefaultHttpLogWriterTest {
 
-    private final TestLogger logger = TestLoggerFactory.getTestLogger(Logbook.class);
+    private final Logger logger = (Logger) LoggerFactory.getLogger(Logbook.class);
+
+    private final ListAppender<ILoggingEvent> listAppender = new ListAppender<>();
+    private final List<ILoggingEvent> logsList = listAppender.list;
+
+    {
+        listAppender.start();
+        logger.addAppender(listAppender);
+    }
 
     @BeforeEach
-    @AfterEach
     void reset() {
-        TestLoggerFactory.clear();
+        logsList.clear();
     }
 
     @Test
     void shouldUseTraceLevelForActivation() {
         final HttpLogWriter unit = new DefaultHttpLogWriter();
 
-        logger.setEnabledLevels(TRACE);
+        logger.setLevel(TRACE);
         assertTrue(unit.isActive());
 
-        logger.setEnabledLevels(INFO);
+        logger.setLevel(INFO);
         assertFalse(unit.isActive());
     }
 
@@ -47,28 +54,24 @@ final class DefaultHttpLogWriterTest {
     void shouldUseTraceLevelForLoggingRequests() throws IOException {
         final HttpLogWriter unit = new DefaultHttpLogWriter();
 
-        logger.setEnabledLevels(TRACE);
+        logger.setLevel(TRACE);
 
         unit.write(new SimplePrecorrelation("", Clock.systemUTC()), "foo");
 
-        final LoggingEvent event = getOnlyElement(logger.getLoggingEvents());
-
-        assertEquals(TRACE, event.getLevel());
-        assertEquals("foo", event.getMessage());
+        assertEquals("foo", logsList.get(0).getMessage());
+        assertEquals(TRACE, logsList.get(0).getLevel());
     }
 
     @Test
     void shouldDefaultToTraceLevelForLoggingResponses() throws IOException {
         final HttpLogWriter unit = new DefaultHttpLogWriter();
 
-        logger.setEnabledLevels(TRACE);
+        logger.setLevel(TRACE);
 
         unit.write(new SimpleCorrelation("1", MIN, MIN), "bar");
 
-        final LoggingEvent event = getOnlyElement(logger.getLoggingEvents());
-
-        assertEquals(TRACE, event.getLevel());
-        assertEquals("bar", event.getMessage());
+        assertEquals("bar", logsList.get(0).getMessage());
+        assertEquals(TRACE, logsList.get(0).getLevel());
     }
 
 }

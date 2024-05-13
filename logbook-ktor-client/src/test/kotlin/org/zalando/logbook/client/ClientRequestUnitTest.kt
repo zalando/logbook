04089360a9af1ac66d91b879e8d9a1b960db3173
Karen Asmarian <karen.asmarian@gmail.com>
2024-05-13 20:13:08 +0200

    issues/1821 retrieve content-type from Ktor's OutgoingContent

--- logbook-ktor-client/src/test/kotlin/org/zalando/logbook/client/ClientRequestUnitTest.kt
+++ logbook-ktor-client/src/test/kotlin/org/zalando/logbook/client/ClientRequestUnitTest.kt
@@ -1,10 +1,13 @@
 package org.zalando.logbook.client
 
 import io.ktor.client.request.HttpRequestBuilder
+import io.ktor.client.request.setBody
 import io.ktor.http.HttpHeaders
+import io.ktor.http.content.OutgoingContent
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import kotlin.text.Charsets.US_ASCII
+import org.zalando.logbook.ContentType.CONTENT_TYPE_HEADER
 
 internal class ClientRequestUnitTest {
 
@@ -16,5 +19,21 @@ internal class ClientRequestUnitTest {
         val request = ClientRequest(req)
         assertThat(request.contentType).isEqualTo("application/json")
         assertThat(request.charset).isEqualTo(US_ASCII)
+    }
+
+    @Test
+    fun `should get content type from request body if not present in headers`() {
+        val req = HttpRequestBuilder().apply {
+            setBody(
+                object : OutgoingContent.ByteArrayContent() {
+                    override val contentType = io.ktor.http.ContentType.Application.Json
+                    override val contentLength = 0L
+                    override fun bytes() = ByteArray(0)
+                }
+            )
+        }
+        val request = ClientRequest(req)
+        assertThat(request.contentType).isEqualTo("application/json")
+        assertThat(request.headers.getFirst(CONTENT_TYPE_HEADER)).isEqualTo("application/json")
     }
 }

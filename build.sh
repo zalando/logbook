fb8808e7c17375e0101430010eb54e02af65e6a3
Sadeq <3616518+msdousti@users.noreply.github.com>
2024-11-17 23:24:56 +0100

    Use run.sh to streamline build

---
+++ build.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+# Define default commands
+COMPILE_CMD="./mvnw compile"
+PACKAGE_CMD="./mvnw package -DskipTests -pl logbook-servlet -am"
+VERIFY_CMD="./mvnw verify -B"
+INSTALL_CMD="./mvnw install -DskipTests -Djacoco.skip=true"
+
+# Flags to track selected options
+COMPILE=false
+NO_TEST_INSTALL=false
+
+# Parse options
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --compile|-c) COMPILE=true ;;
+        --no-test-install|-i) NO_TEST_INSTALL=true ;;
+        -ci|-ic) COMPILE=true; NO_TEST_INSTALL=true ;;
+        *) echo "Unknown option: $1"; exit 1 ;;
+    esac
+    shift
+done
+
+# Execute commands based on the flags
+if $COMPILE; then
+    echo "Running compile..."
+    eval "$COMPILE_CMD"
+fi
+
+if $NO_TEST_INSTALL; then
+    echo "Running install without tests..."
+    eval "$INSTALL_CMD"
+fi
+
+if ! $COMPILE && ! $NO_TEST_INSTALL; then
+    echo "Running default commands..."
+    eval "$PACKAGE_CMD"
+    eval "$VERIFY_CMD"
+fi

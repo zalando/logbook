4b45602adf1f0eff93141d802a20d8f6ecd77e6f
Sadeq Dousti <3616518+msdousti@users.noreply.github.com>
2024-12-09 02:15:20 +0100

    Add package command to build.sh

--- build.sh
+++ build.sh
@@ -10,19 +10,28 @@ INSTALL_CMD="./mvnw install -DskipTests -Djacoco.skip=true"
 # Flags to track selected options
 COMPILE=false
 NO_TEST_INSTALL=false
+PACKAGE=false
 
 # Parse options
 while [[ "$#" -gt 0 ]]; do
     case $1 in
+        --package|-p) PACKAGE=true ;;
         --compile|-c) COMPILE=true ;;
         --no-test-install|-i) NO_TEST_INSTALL=true ;;
         -ci|-ic) COMPILE=true; NO_TEST_INSTALL=true ;;
+        -cp|-pc) PACKAGE=true; COMPILE=true ;;
+        -ip|-pi) PACKAGE=true; NO_TEST_INSTALL=true ;;
         *) echo "Unknown option: $1"; exit 1 ;;
     esac
     shift
 done
 
 # Execute commands based on the flags
+if $PACKAGE; then
+    echo "Running package..."
+    eval "$PACKAGE_CMD"
+fi
+
 if $COMPILE; then
     echo "Running compile..."
     eval "$COMPILE_CMD"
@@ -33,7 +42,7 @@ if $NO_TEST_INSTALL; then
     eval "$INSTALL_CMD"
 fi
 
-if ! $COMPILE && ! $NO_TEST_INSTALL; then
+if ! $COMPILE && ! $NO_TEST_INSTALL && ! $PACKAGE; then
     echo "Running default commands..."
     eval "$PACKAGE_CMD"
     eval "$VERIFY_CMD"

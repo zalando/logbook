12709b6697cac76b2efe68b4c2a39fe4e911d9e1
Karen Asmarian <karen.asmarian@gmail.com>
2025-01-07 20:32:58 +0100

    use MockitoBean instead of MockBean

--- logbook-spring-boot-autoconfigure/src/test/java/org/zalando/logbook/autoconfigure/ExcludeTest.java
+++ logbook-spring-boot-autoconfigure/src/test/java/org/zalando/logbook/autoconfigure/ExcludeTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.verification.VerificationMode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.context.annotation.Bean;
 import org.zalando.logbook.HttpLogWriter;
 import org.zalando.logbook.HttpRequest;
@@ -33,7 +33,7 @@ class ExcludeTest {
     @Autowired
     private Logbook logbook;
 
-    @MockBean
+    @MockitoBean
     private HttpLogWriter writer;
 
     @BeforeEach
